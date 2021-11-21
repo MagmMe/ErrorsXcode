1fee62e5c7c4c9568044fc9602e4dc8c066d3f61
Marcin Magiera <marcin@magm.me>
2021-11-21 13:04:05 +0100

    Added errors to code and handling with it

--- Handling Errors/ContentView.swift
+++ Handling Errors/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+//    #warning("We can force acustom warning using this synthax")
+    
+    var beacons = ["Wellcore", "Estimote", "Xiaomi"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        let beacon1 = beacons[0]
+        let beacon2 = beacons[1]
+        let beacon3 = beacons[2]
+        
+        
+        VStack{
+            Text(beacon1)
+            Text(beacon2)
+            Text(beacon3)
+        }
+        
     }
 }
 
