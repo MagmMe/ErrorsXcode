9b52fac1d350543ef489442678fc19c12ce14ccf
Marcin Magiera <marcin@magm.me>
2021-11-21 09:35:52 +0100

    Initial Commit

---
+++ Handling Errors/Handling_ErrorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Handling_ErrorsApp.swift
+//  Handling Errors
+//
+//  Created by Magme on 21/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Handling_ErrorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
